54ace3dc60e5c4b79f02b2ba9219e13f00c64fc6
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-04-17 21:22:30 +0545

    auth

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
 } = require("./config/config");
 
 const postRoute = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
@@ -31,6 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/posts", postRoute);
+app.use("/api/v1/users", userRouter);
 
 app.listen(process.env.PORT || 6000, () => {
   console.log("running");
