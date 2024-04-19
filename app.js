9a4d43d306d7508134d7b9c8384a109c060754f2
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-04-19 13:23:16 +0545

    redis-before-session

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+
+const redis = require("redis");
+const session = require("express-session");
+
+// const RedisStore = require("connect-redis").default;
+// const session = require("express-session");
+// const { createClient } = require("redis");
+
 const {
   MONGO_IP,
   MONGO_PASSWORD,
   MONGO_PORT,
   MONGO_USER,
+  REDIS_URL,
+  REDIS_PORT,
+  SESSION_SECRET,
 } = require("./config/config");
+
+let RedisStore = require("connect-redis")(session);
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
+// // Initialize client.
+// let redisClient = createClient({
+//   host: REDIS_URL,
+//   port: REDIS_PORT,
+// });
+// redisClient
+//   .connect()
+//   .then(() => {
+//     console.log("Connected to Redis");
+//     // Create and use redisStore here
+//   })
+//   .catch(console.error);
+
+// // Initialize store.
+// let redisStore = new RedisStore({
+//   client: redisClient,
+//   prefix: "myapp:",
+// });
 
 const postRoute = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -15,7 +50,11 @@ const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_
 
 const connectWithRetry = () => {
   mongoose
-    .connect(mongoUrl)
+    .connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
     .then(() => console.log("connected"))
     .catch((e) => {
       console.log(e);
@@ -24,6 +63,35 @@ const connectWithRetry = () => {
 };
 
 connectWithRetry();
+
+// app.use(
+//   session({
+//     store: redisStore,
+//     saveUninitialized: true,
+//     secret: SESSION_SECRET,
+//     cookie: {
+//       secure: false,
+//       resave: false,
+//       saveUninitialized: false,
+//       httpOnly: true,
+//       maxAge: 30000,
+//     },
+//   })
+// );
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
 
 app.use(express.json());
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,8 @@ const User = require("../models/userModel");
 
 const bcrypt = require("bcryptjs");
 exports.signUp = async (req, res) => {
+  const { username, password } = req.body;
   try {
-    const { username, password } = req.body;
     const hashPassword = await bcrypt.hash(password, 12);
 
     const newUser = await User.create({
@@ -31,7 +31,7 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      res.status(404).json({
+      return res.status(404).json({
         status: "fail",
         message: "User not found",
       });
@@ -39,17 +39,17 @@ exports.login = async (req, res) => {
 
     const isCorrect = await bcrypt.compare(password, user.password);
     if (isCorrect) {
-      res.status(200).json({
+      return res.status(200).json({
         status: "success",
       });
     } else {
-      res.status(400).json({
+      return res.status(400).json({
         status: "fail",
         message: "incorrect password",
       });
     }
   } catch (e) {
-    res.status(400).json({
+    return res.status(400).json({
       status: "failed",
     });
   }
